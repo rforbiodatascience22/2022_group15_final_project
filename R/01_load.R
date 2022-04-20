50a196ff8d023b41fc1b972e296224764a1ddcb3
mikkelswartz <s174586@student.dtu.dk>
2022-04-20 09:21:37 +0000

    Cleaned the file

--- R/01_load.R
+++ R/01_load.R
@@ -2,14 +2,14 @@
 library("tidyverse")
 library("fs")
 
-
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
+# Make our own class to deal with the date columns being different from the default
 setClass('myDate')
 setAs("character","myDate", function(from) as.Date(from, format="%d-%b-%y") )
 
-# Load data ---------------------------------------------------------------
+# Load data and wrangle data ----------------------------------------------
 my_data_raw <- read.csv(file = "data/_raw/BRCA2.csv", 
                         na.strings = '',
                         colClasses = c('character',
@@ -29,31 +29,6 @@ my_data_raw <- read.csv(file = "data/_raw/BRCA2.csv",
                                        'myDate',
                                        'factor'))
 
-
-# Wrangle data ------------------------------------------------------------
-#my_data <- my_data_raw %>% 
-#  mutate(Gender_bool = case_when(Gender == 'FEMALE' ~ 1,
-#                                 Gender == 'MALE' ~ 0),
-#         Tumor_Stage_bool = case_when(Tumour_Stage == 'I' ~ 1,
-#                                      Tumour_Stage == 'II' ~ 2,
-#                                      Tumour_Stage == 'III' ~ 3),
-#         ER.status_bool = case_when(ER.status == 'Negative' ~ 0,
-#                                    ER.status == 'Positive' ~ 1),
-#         PR.status_bool = case_when(PR.status == 'Negative' ~ 0,
-#                                    PR.status == 'Positive' ~ 1),
-#         HER2.status_bool = case_when(HER2.status == 'Negative' ~ 0,
-#                                      HER2.status == 'Positive' ~ 1),
-#         
-#         
-#         .keep = "unused")
-
-
-# tumor stage romer --> int
-# dates should not be strings, not dates 
-# booleans 
-
 # Write data --------------------------------------------------------------
 write.csv(x = my_data_raw, 
           file = "data/01_my_data.csv")
-
-
