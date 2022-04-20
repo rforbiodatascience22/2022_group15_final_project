911925247dba750f6c514e66b958a7dcffb36bee
sorensandgaard <s173686@student.dtu.dk>
2022-04-20 10:32:21 +0000

    Achieved models - still need to extract information from the models

--- R/Playground_Documents/Soren_Playground.R
+++ R/Playground_Documents/Soren_Playground.R
@@ -7,8 +7,20 @@
 # Sidenote: I do not have high hopes for good results here, but bad results are still results.
 #################
 
+# Install libraries
+install.packages("patchwork")
+install.packages("fs")
+install.packages("vroom")
+install.packages("broom")
+install.packages("purrr")
+
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("patchwork")
+library("fs")
+library("vroom")
+library("broom")
+library("purrr")
 
 
 # Define functions --------------------------------------------------------
@@ -21,16 +33,28 @@ my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
 
 # Wrangle data ------------------------------------------------------------
 modeldat <- my_data_clean %>% 
-  select(Patient_Status, Protein1, Protein2, Protein3, Protein4)
+  mutate(outcome = case_when(Patient_Status == "Alive" ~ 0,
+                             Patient_Status == "Dead" ~ 1)) %>% 
+  filter(is.na(outcome) == FALSE) %>% 
+  select(outcome, Protein1, Protein2, Protein3, Protein4)
 
 temp <- modeldat %>% 
   pivot_longer(data = .,
-               cols = -Patient_Status,
+               cols = -outcome,
                names_to = "Protein",
                values_to = "Expr_level") %>% 
   group_by(Protein) %>% 
   nest() %>% 
   ungroup()
+
+models <- temp %>% 
+  mutate(mu_group = map(data,
+                        ~glm(outcome ~ Expr_level,
+                             data = .,
+                             family = binomial(link = "logit"))),
+         tidied = map(.x = mu_group,
+                      .f = tidy,
+                      conf.int = TRUE))
 
 # Model data
 my_data_clean %>% ...
