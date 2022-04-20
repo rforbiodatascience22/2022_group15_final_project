1080fd093a7352816b7f9efd123b85ab5107a7b2
helenewegener1 <s165827@student.dtu.dk>
2022-04-20 08:58:14 +0000

    Helene - data load

--- R/01_load.R
+++ R/01_load.R
@@ -8,15 +8,53 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_raw <- read.csv(file = "data/_raw/BRCA2.csv", na.strings = '')
+my_data_raw <- read.csv(file = "data/_raw/BRCA2.csv", 
+                        na.strings = '',
+                        colClasses = c('character',
+                                       'numeric',
+                                       'factor',
+                                       'numeric',
+                                       'numeric',
+                                       'numeric',
+                                       'numeric',
+                                       'factor',
+                                       'factor',
+                                       'factor',
+                                       'factor',
+                                       'factor',
+                                       'factor',
+                                       'character',
+                                       'character',
+                                       'factor'))
+
+my_data_raw %>% 
+  count(Tumour_Stage == 'III')
 
 
 # Wrangle data ------------------------------------------------------------
-my_data <- my_data_raw %>% 
+#my_data <- my_data_raw %>% 
+#  mutate(Gender_bool = case_when(Gender == 'FEMALE' ~ 1,
+#                                 Gender == 'MALE' ~ 0),
+#         Tumor_Stage_bool = case_when(Tumour_Stage == 'I' ~ 1,
+#                                      Tumour_Stage == 'II' ~ 2,
+#                                      Tumour_Stage == 'III' ~ 3),
+#         ER.status_bool = case_when(ER.status == 'Negative' ~ 0,
+#                                    ER.status == 'Positive' ~ 1),
+#         PR.status_bool = case_when(PR.status == 'Negative' ~ 0,
+#                                    PR.status == 'Positive' ~ 1),
+#         HER2.status_bool = case_when(HER2.status == 'Negative' ~ 0,
+#                                      HER2.status == 'Positive' ~ 1),
+#         
+#         
+#         .keep = "unused")
 
+
+# tumor stage romer --> int
+# dates should not be strings, not dates 
+# booleans 
 
 # Write data --------------------------------------------------------------
-write.csv(x = my_data,
+write.csv(x = my_data, 
           file = "data/01_my_data.csv")
 
 
