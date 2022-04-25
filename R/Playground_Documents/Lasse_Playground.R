62686adac3f23f819f5a1ad93e53f96ac6ac3fdc
LasseRene <s183448@student.dtu.dk>
2022-04-25 09:47:14 +0000

    Lasse Playground update. New augments almost ready for augment file. New plots with average expression.

--- R/Playground_Documents/Lasse_Playground.R
+++ R/Playground_Documents/Lasse_Playground.R
@@ -61,7 +61,39 @@ my_data_clean %>%
     x = Patient_Status,
     y = Surgery_type,
     color = Tumour_Stage)) + 
-  geom_jitter()
+  geom_point()
+
+
+library("dplyr")
+#Creating average expression variable
+clean2 <- my_data_clean %>% 
+  (mutate(average_expression = ((my_data_clean[,"Protein1"]) + (my_data_clean[,"Protein2"]) + (my_data_clean[,"Protein3"]) + (my_data_clean[,"Protein4"]))/4)) %>% 
+  as_tibble()
+
+avg_vector <- (average_expression = ((my_data_clean[,"Protein1"]) + (my_data_clean[,"Protein2"]) + (my_data_clean[,"Protein3"]) + (my_data_clean[,"Protein4"]))/4) %>% 
+  as_tibble()
+clean3 <- tibble(my_data_clean,avg_vector)
+
+clean2[,"average_expression"] %>% as_tibble()
+as_tibble(clean2[,"average_expression"],validate=TRUE)
+
+clean3 %>% filter(,Tumour_Stage == "III") %>% 
+  ggplot(mapping = aes(
+    x = Protein1,
+    y = Protein4,
+    color=average_expression)) +
+  scale_y_continuous(expand = c(0,0)) + 
+  geom_smooth()
+
+
+clean4 %>% group_by(Tumour_Stage) %>%  ggplot(aes(x  = Protein1, fill = Tumour_Stage)) + 
+  geom_histogram()
+clean4
+
+#clean4 <- clean3 %>% filter(Date_of_Last_Visit < '2022-01-01')
+
+Augment_var = my_data_clean_aug %>% filter(,Tumour_Stage == "III") %>% filter(,Histology == "Infiltrating Ductal Carcinoma")
+
 
 # Write data --------------------------------------------------------------
 write_tsv(...)
