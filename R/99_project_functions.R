c1c556d769daae137c0f37f45ac3034b03449061
helenewegener1 <s165827@student.dtu.dk>
2022-04-25 10:00:29 +0000

    Helene added a histrogram function to 99_project_functions.R

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -6,3 +6,25 @@ bar <- function(x){
   return(x^2)
 }
 ...
+
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+library("fs")
+library("patchwork")
+
+my_data_clean_aug <- read.csv(file = "data/03_my_data_clean_aug.csv")
+
+
+# Plotting an atribute 
+histrogram_count <- function(data, atribute){
+  output <- ggplot(data, mapping = aes(x = {{atribute}})) + 
+    geom_bar(fill = 'dodgerblue4') + 
+    labs(y = 'Count')
+    theme_minimal(base_family = 'Avenir',
+                  base_size = 10) 
+  
+  return(output)
+}
+
+histrogram_count(data = my_data_clean_aug, atribute = Patient_Status)
+
