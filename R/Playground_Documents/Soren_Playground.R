c4c552c14a3220d5d470b26acda3cbd6b426da59
sorensandgaard <s173686@student.dtu.dk>
2022-04-27 09:18:17 +0000

    Updated the visual quality of the Manhattan Plot from the logistic regression.

--- R/Playground_Documents/Soren_Playground.R
+++ R/Playground_Documents/Soren_Playground.R
@@ -52,20 +52,58 @@ models <- temp %>%
                       .f = tidy,
                       conf.int = TRUE)) %>% 
   unnest(tidied) %>% 
-  filter(term != "(Intercept)")
+  filter(term != "(Intercept)") %>% 
+  mutate(Significant = case_when(p.value < 0.05 ~ TRUE,
+                                 p.value >= 0.05 ~ FALSE),
+         neglog10p = -log10(p.value))
 
 ## Visualise data
 models %>% 
-  select(Protein,p.value) %>% 
+  select(Protein,neglog10p,Significant) %>% 
   ggplot(data = .,
          mapping = aes(x = Protein,
-                       y = p.value,
-                       color = p.value)) + 
-  geom_point(mapping = aes(color = p.value)) + 
-  geom_hline(mapping = aes(yintercept = 0.05),
-             color = "Red",
-             alpha = 0.5) + 
-  theme_classic()
+                       y = neglog10p)) + 
+  geom_point(mapping = aes(color = Significant),
+             size = 2,
+             alpha = 1) + 
+  geom_hline(mapping = aes(yintercept = -log10(0.05)),
+             color = "Black",
+             alpha = 0.5,
+             linetype = "dashed") + 
+  geom_polygon(mapping = aes(x=c(0.4,0.4,4.6,4.6),
+                             y=c(-log10(0.05),1.75,1.75,-log10(0.05))),
+               fill = "Green",
+               alpha = 0.1) +
+  geom_polygon(mapping = aes(x=c(0.4,0.4,4.6,4.6),
+                             y=c(-0,-log10(0.05),-log10(0.05),0)),
+               fill = "Red",
+               alpha = 0.1) +
+  annotate(geom = "text",
+           x=1,
+           y=1.7,
+           label="Significant",
+           size = 2,
+           color="Darkgreen") +
+  annotate(geom = "text",
+           x=1,
+           y=1.25,
+           label="Insignificant",
+           size = 2,
+           color="Darkred") +
+  theme_classic() + 
+  theme(axis.text.x = element_text(angle = 90, 
+                                   vjust = 0.5, 
+                                   hjust=1),
+        legend.position = "bottom") +
+  labs(x = "Protein",
+       y = "Negative log10 of p-value",
+       title = "Manhattan plot")
+
+ggsave(filename = "ManhattanPlot.png",
+       width = 2.5,
+       height = 4,
+       units = "in",
+       path = "R/Playground_Documents")
 
 # Protein expression vs. Cancer type, density map
 BRCA_data_long <- my_data_raw %>%
