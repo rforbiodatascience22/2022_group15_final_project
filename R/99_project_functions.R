109101296f701ae0fda389b3360b5314076dee98
sorensandgaard <s173686@student.dtu.dk>
2022-05-02 07:49:54 +0000

    Added density plot function for dynamic protein selection vs attribute

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -18,4 +18,21 @@ histrogram_count <- function(data, atribute){
 
 histrogram_count(data = my_data_clean_aug, atribute = Patient_Status)
 
+# Plotting protein expression vs. type of cancer
+dens_protein_BRCA <- function(data, proteins, attribute){
+  data %>%
+    select({{proteins}},{{attribute}}) %>%
+    pivot_longer(cols = 1:length({{proteins}}),
+                 names_to = 'Protein',
+                 values_to = 'Expression_Level') %>% 
+    ggplot(data = .,
+           mapping = aes(x = Expression_Level,
+                         color = {{attribute}})) + 
+    geom_density() + 
+    facet_wrap(~Protein,
+               nrow=4) +
+    theme_classic()
+}
+
+
 
