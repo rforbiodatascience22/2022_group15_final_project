7e176e4ea8d782085a676915dd096f1c2ab2e0de
helenewegener1 <s165827@student.dtu.dk>
2022-05-02 08:05:47 +0000

    Helene adds Age group and Death month columns to the data. in the augment file

--- R/Playground_Documents/Helene_Playground.R
+++ R/Playground_Documents/Helene_Playground.R
@@ -20,9 +20,7 @@ my_data_clean_aug %>%
 
 # Add column of death month.
 # Most people die in winter time. 
-my_data_clean_aug %>% mutate(Death_Month = 
-                               case_when(Patient_Status == 'Dead' ~ 
-                                           str_sub(Date_of_Last_Visit, 6, 7))) %>%
+my_data_clean_aug %>%
   filter(!is.na(Death_Month)) %>% 
   ggplot(mapping = aes(x = Death_Month)) +
   geom_bar() + 
@@ -151,20 +149,6 @@ ggsave(filename = 'helene_playground_Time_Diff_plot.png',
 
 # Recreating plots
 my_data_clean_aug %>% 
-  mutate(Age_Group = case_when(25 <= Age & Age < 30 ~ '25 to 30',
-                               30 <= Age & Age < 35 ~ '30 to 35',
-                               35 <= Age & Age < 40 ~ '35 to 40',
-                               40 <= Age & Age < 45 ~ '40 to 45',
-                               45 <= Age & Age < 50 ~ '45 to 50',
-                               50 <= Age & Age < 55 ~ '50 to 55',
-                               55 <= Age & Age < 60 ~ '55 to 60',
-                               60 <= Age & Age < 65 ~ '60 to 65',
-                               65 <= Age & Age < 70 ~ '65 to 70',
-                               70 <= Age & Age < 75 ~ '70 to 75',
-                               75 <= Age & Age < 80 ~ '75 to 80',
-                               80 <= Age & Age < 85 ~ '80 to 85',
-                               85 <= Age & Age < 90 ~ '85 to 90',
-                               90 <= Age & Age < 95 ~ '90 to 95')) %>% 
   ggplot(mapping = aes(x = Age_Group,
                        fill = Histology)) +
   geom_bar() +
