47b1346e0206628002ae65c551d55cb47df29cbc
helenewegener1 <s165827@student.dtu.dk>
2022-05-04 10:30:24 +0000

    Helene went thourgh logistic regression and did small corrections.

--- R/06_analysis_logistic_regression.R
+++ R/06_analysis_logistic_regression.R
@@ -7,12 +7,17 @@ library("cowplot")
 source(file = "/cloud/project/R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_csv(file = "/cloud/project/data/03_my_data_clean_aug.csv")
+my_data_clean_aug <- read_csv(file = "/cloud/project/data/03_my_data_clean_aug.csv",
+                              show_col_types = FALSE)
 
 
 # Wrangle data ------------------------------------------------------------
 models <- my_data_clean_aug %>% 
-  select(Patient_Status_Binary,Protein1, Protein2, Protein3, Protein4) %>% 
+  select(Patient_Status_Binary,
+         Protein1, 
+         Protein2,
+         Protein3, 
+         Protein4) %>% 
   pivot_longer(data = .,
                cols = -Patient_Status_Binary,
                names_to = "Protein",
@@ -67,24 +72,21 @@ models %>%
            label="Insignificant",
            size = 2,
            color="Darkred") +
-  theme_classic() + 
-  theme(axis.text.x = element_text(angle = 90, 
-                                   vjust = 0.5, 
-                                   hjust=1),
-        legend.position = "bottom") +
-  labs(x = "Protein",
+  our_theme(legend_position = "bottom",
+            x_angle = 45) +
+  labs(x = "",
        y = "Negative log10 of p-value",
        title = "Manhattan plot")
 
-
-# Write data --------------------------------------------------------------
-ggsave(filename = "Manhattan_Plot_Logistic.png",
+ggsave(filename = "manhattan_plot_logistic_regression.png",
        width = 2.5,
        height = 4,
        units = "in",
        path = "/cloud/project/results")
 
+# p-value table
 models %>% 
-  select(-data,-mu_group) %>% 
+  select(-data, 
+         -mu_group) %>% 
   write_csv(x = .,
             file = "/cloud/project/results/p-value_table.csv")
