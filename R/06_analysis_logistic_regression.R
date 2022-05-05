b1858dbd8200b98b6933b8257678c6766f928678
aldisbjorgvinsdottir <s212956@dtu.dk>
2022-05-05 06:34:05 +0000

    Looking at data

--- R/06_analysis_logistic_regression.R
+++ R/06_analysis_logistic_regression.R
@@ -7,12 +7,17 @@ library("cowplot")
 source(file = "/cloud/project/R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_csv(file = "/cloud/project/data/03_my_data_clean_aug.csv")
+my_data_clean_aug <- read_csv(file = "/cloud/project/data/03_my_data_clean_aug.csv",
+                              show_col_types = FALSE)
 
 
 # Wrangle data ------------------------------------------------------------
 models <- my_data_clean_aug %>% 
-  select(Patient_Status_Binary,Protein1, Protein2, Protein3, Protein4) %>% 
+  select(Patient_Status_Binary,
+         Protein1, 
+         Protein2,
+         Protein3, 
+         Protein4) %>% 
   pivot_longer(data = .,
                cols = -Patient_Status_Binary,
                names_to = "Protein",
@@ -36,7 +41,9 @@ models <- my_data_clean_aug %>%
 
 # Visualise data ----------------------------------------------------------
 models %>% 
-  select(Protein,neglog10p,Significant) %>% 
+  select(Protein,
+         neglog10p,
+         Significant) %>% 
   ggplot(data = .,
          mapping = aes(x = Protein,
                        y = neglog10p)) + 
@@ -47,44 +54,41 @@ models %>%
              color = "Black",
              alpha = 0.5,
              linetype = "dashed") + 
-  geom_polygon(mapping = aes(x=c(0.4,0.4,4.6,4.6),
-                             y=c(-log10(0.05),1.75,1.75,-log10(0.05))),
+  geom_polygon(mapping = aes(x = c(0.4,0.4,4.6,4.6),
+                             y = c(-log10(0.05),1.75,1.75,-log10(0.05))),
                fill = "Green",
                alpha = 0.1) +
-  geom_polygon(mapping = aes(x=c(0.4,0.4,4.6,4.6),
-                             y=c(-0,-log10(0.05),-log10(0.05),0)),
+  geom_polygon(mapping = aes(x = c(0.4,0.4,4.6,4.6),
+                             y = c(-0,-log10(0.05),-log10(0.05),0)),
                fill = "Red",
                alpha = 0.1) +
   annotate(geom = "text",
-           x=1,
-           y=1.7,
-           label="Significant",
+           x = 1,
+           y = 1.7,
+           label = "Significant",
            size = 2,
-           color="Darkgreen") +
+           color = "Darkgreen") +
   annotate(geom = "text",
            x=1,
            y=1.25,
-           label="Insignificant",
+           label = "Insignificant",
            size = 2,
-           color="Darkred") +
-  theme_classic() + 
-  theme(axis.text.x = element_text(angle = 90, 
-                                   vjust = 0.5, 
-                                   hjust=1),
-        legend.position = "bottom") +
-  labs(x = "Protein",
+           color = "Darkred") +
+  our_theme(legend_position = "bottom",
+            x_angle = 45) +
+  labs(x = "",
        y = "Negative log10 of p-value",
        title = "Manhattan plot")
 
-
-# Write data --------------------------------------------------------------
-ggsave(filename = "Manhattan_Plot_Logistic.png",
-       width = 2.5,
-       height = 4,
+ggsave(filename = "manhattan_plot_logistic_regression.png",
+       width = 4,
+       height = 3,
        units = "in",
        path = "/cloud/project/results")
 
+# p-value table
 models %>% 
-  select(-data,-mu_group) %>% 
+  select(-data, 
+         -mu_group) %>% 
   write_csv(x = .,
             file = "/cloud/project/results/p-value_table.csv")
